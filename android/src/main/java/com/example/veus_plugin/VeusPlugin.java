e99f913d6331f42d59d82ef9609cadfc91ea146a
Vlad Lotysh <gangsta1@i.ua>
2020-11-09 23:07:19 +0200

    Added get contact

--- android/src/main/java/com/example/veus_plugin/VeusPlugin.java
+++ android/src/main/java/com/example/veus_plugin/VeusPlugin.java
@@ -3,9 +3,16 @@ package com.example.veus_plugin;
 import android.annotation.TargetApi;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Build;
+import android.provider.ContactsContract;
 
 import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -17,69 +24,129 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** VeusPlugin */
+/**
+ * VeusPlugin
+ */
 
 @TargetApi(Build.VERSION_CODES.ECLAIR)
 public class VeusPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
-  private ContentResolver contentResolver;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    private ContentResolver contentResolver;
 
-  private void initInstance(BinaryMessenger messenger, Context context) {
-    channel = new MethodChannel(messenger, "veus_plugin");
-    channel.setMethodCallHandler(this);
-    this.contentResolver = context.getContentResolver();
-  }
+    private static final String[] PROJECTION =
+            {
+                    ContactsContract.Data.CONTACT_ID,
+                    ContactsContract.Profile.DISPLAY_NAME,
+                    ContactsContract.Contacts.Data.MIMETYPE,
+                    ContactsContract.RawContacts.ACCOUNT_TYPE,
+                    ContactsContract.RawContacts.ACCOUNT_NAME,
+                    ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+            };
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    initInstance(flutterPluginBinding.getBinaryMessenger(), flutterPluginBinding.getApplicationContext());
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success(getAndroidVersion());
-    } else {
-      result.notImplemented();
+    private void initInstance(BinaryMessenger messenger, Context context) {
+        channel = new MethodChannel(messenger, "veus_plugin");
+        channel.setMethodCallHandler(this);
+        this.contentResolver = context.getContentResolver();
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-    channel = null;
-    contentResolver = null;
-  }
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        initInstance(flutterPluginBinding.getBinaryMessenger(), flutterPluginBinding.getApplicationContext());
+    }
 
-  @Override
-  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
-    // TODO: your plugin is now attached to an Activity
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success(getAndroidVersion());
+        } else if (call.method.equals("getContacts")) {
+            result.success(getAndroidVersion());
+        } else {
+            result.notImplemented();
+        }
+    }
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-    // TODO: the Activity your plugin was attached to was
-    // destroyed to change configuration.
-    // This call will be followed by onReattachedToActivityForConfigChanges().
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+        contentResolver = null;
+    }
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
-    // TODO: your plugin is now attached to a new Activity
-    // after a configuration change.
-  }
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+        // TODO: your plugin is now attached to an Activity
+    }
 
-  @Override
-  public void onDetachedFromActivity() {
-    // TODO: your plugin is no longer associated with an Activity.
-    // Clean up references.
-  }
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        // TODO: the Activity your plugin was attached to was
+        // destroyed to change configuration.
+        // This call will be followed by onReattachedToActivityForConfigChanges().
+    }
 
-  private String getAndroidVersion() {
-    return "Android == " + android.os.Build.VERSION.RELEASE;
-  }
+    @Override
+    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+        // TODO: your plugin is now attached to a new Activity
+        // after a configuration change.
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        // TODO: your plugin is no longer associated with an Activity.
+        // Clean up references.
+    }
+
+    private String getAndroidVersion() {
+        return "Android == " + android.os.Build.VERSION.RELEASE;
+    }
+
+    private String getContacts() {
+        return "Text";
+    }
+
+    @TargetApi(Build.VERSION_CODES.CUPCAKE)
+    private class GetContactsTask extends AsyncTask<Object, Void, ArrayList<HashMap>> {
+
+        private String callMethod;
+        private Result getContactResult;
+
+        public GetContactsTask(String callMethod, Result result) {
+            this.callMethod = callMethod;
+            this.getContactResult = result;
+        }
+
+
+        @Override
+        protected ArrayList<HashMap> doInBackground(Object... params) {
+
+
+            return null;
+        }
+
+        private Cursor getCursor(String query, String rawContactId) {
+            String selection = "(" + ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=? OR "
+                    + ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=? OR "
+                    + ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=? OR "
+                    + ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.RawContacts.ACCOUNT_TYPE + "=?" + ")";
+
+            ArrayList<String> selectionArgs = new ArrayList<>(Arrays.asList(ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE,
+                    ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE, ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE, ContactsContract.RawContacts.ACCOUNT_TYPE));
+
+            if (query != null) {
+                selectionArgs = new ArrayList<>();
+                selectionArgs.add(query + "%");
+                selection = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?";
+            }
+
+            return contentResolver.query(ContactsContract.Data.CONTENT_URI, PROJECTION, selection, selectionArgs.toArray(new String[selectionArgs.size()]), null);
+
+        }
+    }
+
 }
+
